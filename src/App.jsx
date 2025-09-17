ce9f8387241c631a6017dae0b09f7d1f3c38d129
Akash <akashkrchoudhary007@gmail.com>
2025-09-17 13:46:18 +0530

    setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='flex justify-center items-center bg-zinc-800 h-lvh'>
+      <h1 className='text-7xl text-orange-500 font-bold'> Jai Shree Ram</h1>
+      
+    </div>
+  )
+}
