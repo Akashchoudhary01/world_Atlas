83524fbe5b9f3991ef55b5a191410daf571cd577
Akash <akashkrchoudhary007@gmail.com>
2025-09-27 22:35:36 +0530

    '

--- src/Pages/CountryCard.jsx
+++ src/Pages/CountryCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {NavLink} from 'react-router-dom'
 export default function CountryCard({ country }) {
   const { flags, name, population, capital, region } = country;
 
@@ -31,6 +31,10 @@ export default function CountryCard({ country }) {
             <span className="font-semibold">Capital:</span> {capital}
           </li>
         </ul>
+        <NavLink to={`/contact/${name.common}`}>
+      <button className="px-3 pt-.8 text-sm rounded-md m-auto p-2 outline-1">Show more</button>
+
+        </NavLink>
       </div>
     </li>
   );
